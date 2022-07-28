a4683acb6f814bd6a520b0f472758af03a3d7b45
Ryan Chang <anakinchung930@gmail.com>
2022-07-28 19:35:53 +0800

    Update App.vue

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,5 @@
 <template>
-<div v-show="message.length>0" class="fixed z-50 w-full top-36 left-0">
+    <div v-show="message.length>0" class="fixed z-50 w-full top-36 left-0">
         <div class="mx-auto w-2/3 py-5 px-10 bg-orange-500 text-2xl opacity-90 font-medium shadow text-white rounded">
             <div><span class="material-icons mr-3">notification_important</span>{{message}}</div>
         </div>
